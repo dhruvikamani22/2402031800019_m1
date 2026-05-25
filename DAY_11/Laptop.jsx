0bcf4c72412d3cb3ea1fbcb948a66801104efadf
dhruvikamani22 <2402031800019@silveroakuni.ac.in>
2026-05-25 14:22:50 +0530

    Create Laptop.jsx

---
+++ DAY_11/Laptop.jsx
@@ -0,0 +1,13 @@
+export default function Laptop(){
+    return (
+        <div>
+            <h2>Laptop list:</h2>
+            <ul>
+                <li>Apple macbook</li>
+                <li>Samsung notebook</li>
+                <li>Dell inspiron</li>
+
+            </ul>
+        </div>
+    )
+}
