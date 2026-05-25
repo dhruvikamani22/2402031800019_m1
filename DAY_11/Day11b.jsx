da7a680dc5dc4961907afb6dbfe3dab77dc3b97a
dhruvikamani22 <2402031800019@silveroakuni.ac.in>
2026-05-25 11:02:33 +0530

    Create Day11b.jsx

--- DAY_11/Day11b.jsx
+++ DAY_11/Day11b.jsx
@@ -1,9 +1,14 @@
-function Day11b({name="Guest", age=18, city="Unknown"}){
+function Day11b({name="Guest", age=18, city="Unknown",hobbies}){
     return (
         <div>
             <h2>Hello, {name}</h2>
             <p>Age: {age}</p>
             <p>City: {city} </p>
+            <ul>
+                {hobbies.map((hobby,indesx)=>(
+                    <li key={indesx}>{hobby}</li>
+                ))}
+            </ul>
         </div>
     )
 
@@ -14,12 +19,13 @@ export default Day11b
 
 // import Day11b from "./Day11b"
 // function App() {
- 
+//   const hobbies = ["Reading", "coding", "Travelling"]
 
 //   return (
 //     <>
-//     <Day11b name="Dhruvi kamani" age={20} city="junagadh"/>
-//     <Day11b name="Dhruvi"/>
+//     <Day11b name="Dhruvi kamani" age={20} city="junagadh" hobbies={hobbies}/>
+//     {/* <Day11b name="Dhruvi"/> */}
+
 //     </>
 //   )
 // }
