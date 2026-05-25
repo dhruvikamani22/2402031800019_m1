4ee6f79e3ca7f5af66c0c9170bba8c812aa07d5d
dhruvikamani22 <2402031800019@silveroakuni.ac.in>
2026-05-25 10:46:36 +0530

    Create Day11b.jsx

---
+++ DAY_11/Day11b.jsx
@@ -0,0 +1,27 @@
+function Day11b({name="Guest", age=18, city="Unknown"}){
+    return (
+        <div>
+            <h2>Hello, {name}</h2>
+            <p>Age: {age}</p>
+            <p>City: {city} </p>
+        </div>
+    )
+
+}
+export default Day11b
+
+// App.jsx
+
+// import Day11b from "./Day11b"
+// function App() {
+ 
+
+//   return (
+//     <>
+//     <Day11b name="Dhruvi kamani" age={20} city="junagadh"/>
+//     <Day11b name="Dhruvi"/>
+//     </>
+//   )
+// }
+
+// export default App
